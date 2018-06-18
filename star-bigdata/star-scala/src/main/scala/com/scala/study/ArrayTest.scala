b2274f85bf1cd9689e5f414c879023a1eac66219
qianli8811 <624003618@qq.com>
2018-06-19 00:08:15 +0800

    scala

--- star-bigdata/star-scala/src/main/scala/com/scala/study/ArrayTest.scala
+++ star-bigdata/star-scala/src/main/scala/com/scala/study/ArrayTest.scala
@@ -4,7 +4,7 @@ import scala.collection.mutable.ArrayBuffer
 
 object ArrayTest {
   def main(args: Array[String]): Unit = {
-    var arr1 = new Array[Int](10);
+    /*var arr1 = new Array[Int](10);
     arr1(0) = 1;
     arr1(1) = 2;
     arr1(2) = 3;
@@ -18,6 +18,14 @@ object ArrayTest {
     b.insert(3,7);
     println(b.trimEnd(4))
 
+    var student = new Student();
+    student.age_=(20);
+    println( student.age)*/
+
+    val p1 = new Person("leo")
+    p1.sayHello
+    val p2 = new Person("jack") with MyLogged
+    p2.sayHello
   }
 
 }
