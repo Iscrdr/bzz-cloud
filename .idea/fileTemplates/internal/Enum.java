ec9a0461df383d3871dc3a268826071755c9261b
qianli8811 <624003618@qq.com>
2018-11-23 00:04:38 +0800

    项目更名：star-cloud ----> bzz-cloud

---
+++ .idea/fileTemplates/internal/Enum.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @program: ${PROJECT_NAME}
+ * @description: ${description}
+ * @author: Yang qianli
+ * @create: ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+ * @version: 1.0.0
+ */
+public enum ${NAME} {
+}
---
+++ bzz-caches/bzz-redis/src/test/java/com/bzz/cloud/AppTest.java
@@ -0,0 +1,10 @@
+package com.bzz.cloud;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+}
